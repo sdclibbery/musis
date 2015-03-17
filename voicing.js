570bba63daeae5f4eaa10348ab1b9753f6d103e5
sdclibbery <sdclibbery@googlemail.com>
2015-03-17 00:08:41 +0000

    very basic voice assignment

--- voicing.js
+++ voicing.js
@@ -3,13 +3,26 @@
 // Domain
 
 var makeChordComposer = function (metronome, play, note, stars, pitchClasses) {
+  var notes = [];
+  var octave = 3;
+  var lastFreq = 0;
+  for (i = 0; i < pitchClasses.length; i++) {
+    var pc = pitchClasses[i];
+    var freq = note.freq(pc, octave);
+    if (freq/lastFreq < Math.pow(2, 3/12)) {
+      octave++;
+      freq = note.freq(pc, octave);
+    }
+    notes.push({freq: freq, pitchClass: pc});
+    lastFreq = freq;
+  }
+
   return function () {
     var time = metronome.nextBeatAt();
     var duration = metronome.beatDuration();
-    pitchClasses.map(function (pitchClass) {
-      var freq = note.freq(pitchClass, 4);
-      play.note(time, freq, duration);
-      stars.burst(pitchClass);
+    notes.map(function (note) {
+      play.note(time, note.freq, duration);
+      stars.burst(note.pitchClass);
     });
   };
 };
@@ -29,7 +42,7 @@ musis.voicing.prototype.update = function (metronome, play, note, stars) {
 
   var time = metronome.nextBeatAt();
   var duration = metronome.beatDuration();
-  if (this.lastPlayedTime < time) {
+  if (this.lastPlayedTime + duration - 0.05 <= time) {
     this.lastPlayedTime = time;
     if (this.composer) { this.composer(); }
   }
