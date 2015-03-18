89f6b030e2ce506cb259575186ab5cbe6a52a948
sdclibbery <sdclibbery@googlemail.com>
2015-03-18 21:40:09 +0000

    refactor voicing

--- voicing.js
+++ voicing.js
@@ -11,32 +11,40 @@ var ranges = {
   soprano: { low: new musis.note("C", 4), high: new musis.note("C", 6) }
 };
 
-var voiceNote = function (voice, pitchClass) {
-  var note = new musis.note(pitchClass, 1);
-  while (note.isLowerThan(ranges[voice].low)) {
-    note = new musis.note(pitchClass, note.octave+1);
-  }
-  return note;
-};
-
 var makeChordComposer = function (play, stars, pitchClasses) {
-  var num = pitchClasses.length;
-  for (var i = num; i < voices.length; i++) {
-    pitchClasses[i] = pitchClasses[i-num];
-  }
-
-  var notes = [];
-  for (var i = 0; i < pitchClasses.length; i++) {
-    var pc = pitchClasses[i];
-    notes.push(voiceNote(voices[i % voices.length], pc));
-  }
-
+  pitchClasses = expandPCs(pitchClasses);
+  var notes = assignToVoices(pitchClasses);
   return function (time, duration) {
     notes.map(function (note) {
       play.note(time, note.freq(), duration);
       stars.burst(note.pitchClass);
     });
   };
+};
+
+var expandPCs = function (pitchClasses) {
+  var num = pitchClasses.length;
+  for (var i = num; i < voices.length; i++) {
+    pitchClasses[i] = pitchClasses[i-num];
+  }
+  return pitchClasses;
+};
+
+var assignToVoices = function (pitchClasses) {
+  var notes = [];
+  for (var i = 0; i < pitchClasses.length; i++) {
+    var pc = pitchClasses[i];
+    notes.push(voiceNote(voices[i % voices.length], pc));
+  }
+  return notes;
+};
+
+var voiceNote = function (voice, pitchClass) {
+  var note = new musis.note(pitchClass, 1);
+  while (note.isLowerThan(ranges[voice].low)) {
+    note = new musis.note(pitchClass, note.octave+1);
+  }
+  return note;
 };
 
 //////
