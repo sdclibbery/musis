38a9dff7a273d7da216b904976f4e9140dd3643d
sdclibbery <sdclibbery@googlemail.com>
2015-03-21 18:06:42 +0000

    move pre-compose ops out into music module

--- music/compose.js
+++ music/compose.js
@@ -2,21 +2,9 @@
 
 // Domain
 
-var expandPCs = function (pitchClasses) {
-  var num = pitchClasses.length;
-  for (var i = num; i < 4; i++) { // at least 4 parts
-    pitchClasses[i] = pitchClasses[i-num];
-  }
-  return pitchClasses;
-};
-
-//////
-
 musis.compose = {};
 
-musis.compose.blockChords = function (play, stars, pitchClasses) {
-  pitchClasses = expandPCs(pitchClasses);
-  var notes = musis.voicing.assignToVoices(pitchClasses);
+musis.compose.blockChords = function (play, stars, notes) {
   return function (time, duration) {
     notes.map(function (note) {
       play.note(time, note.freq(), duration);
--- music/music.js
+++ music/music.js
@@ -7,12 +7,18 @@ musis.music = function () {
   this.lastBeatAt = 0;
 };
 
+musis.music.prototype.nextHarmony = function (pitchClasses) {
+  this.nextPitchClasses = pitchClasses;
+};
+
+musis.music.prototype.update = function (metronome, play, stars) {
+};
+
 musis.music.prototype.update = function (metronome, play, stars) {
   if (this.nextPitchClasses.length > 0) {
-    this.composer = musis.compose.blockChords(play, stars, this.nextPitchClasses);
+    this.compose(play, stars);
     this.nextPitchClasses = [];
   }
-
   var nextBeatAt = metronome.nextBeatAt();
   var timeToNextBeat = nextBeatAt - play.timeNow();
   var duration = metronome.beatDuration();
@@ -22,8 +28,18 @@ musis.music.prototype.update = function (metronome, play, stars) {
   }
 };
 
-musis.music.prototype.nextHarmony = function (pitchClasses) {
-  this.nextPitchClasses = pitchClasses;
+musis.music.prototype.compose = function (play, stars) {
+  var pitchClasses = expandPCs(this.nextPitchClasses);
+  var notes = musis.voicing.assignToVoices(pitchClasses);
+  this.composer = musis.compose.blockChords(play, stars, notes);
+};
+
+var expandPCs = function (pitchClasses) {
+  var num = pitchClasses.length;
+  for (var i = num; i < 4; i++) { // at least 4 parts
+    pitchClasses[i] = pitchClasses[i-num];
+  }
+  return pitchClasses;
 };
 
 })();
