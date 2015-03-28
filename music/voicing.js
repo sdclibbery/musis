89f0cb10eec6b7c27def02db94e87c5a4f95fbf0
sdclibbery <sdclibbery@googlemail.com>
2015-03-28 18:19:36 +0000

    generate notes, with all possible octaves

--- music/voicing.js
+++ music/voicing.js
@@ -23,13 +23,16 @@ musis.voicing.assignToVoices = function (pitchClasses) {
       return comb.indexOf(pc) >= 0;
     });
   };
-  var toNotes = function (comb, res) {
-    // pc-to-note, expanding combinations to include all octaves within each voices range
-    //res.push(comb.map);
+  var toNotes = function (res, comb) {
+    var bs = notesWithinRange(comb[0], ranges.bass);
+    var ts = notesWithinRange(comb[1], ranges.tenor);
+    var as = notesWithinRange(comb[2], ranges.alto);
+    var ss = notesWithinRange(comb[3], ranges.soprano);
+    combinations([bs, ts, as, ss]).map(function (notes) { res.push(notes); });
     return res;
   };
   var addScore = function (comb) { return { voices: comb, score: 0 }; };
-  var combs = combinations(pcs, 3) // get combinations for the upper three voices
+  var combs = combinations([pcs, pcs, pcs]) // get combinations for the upper three voices; bass is always first
     .map(insertBass)
     .filter(allPCsPresent)
     .reduce(toNotes, [])
@@ -51,20 +54,30 @@ musis.voicing.assignToVoices = function (pitchClasses) {
   return [];
 };
 
-var combinations = function (xs, n) {
-  var num = xs.length;
+var notesWithinRange = function (pc, range) {
+  var note = new musis.note(pc, range.low.octave).above(range.low);
+  var res = [];
+  while (!note.isHigherThan(range.high)) {
+    res.push(note);
+    note.octave++;
+  }
+  return res;
+};
+
+var combinations = function (xss) { // Takes an array with one entry per slot in the output. Each entry is another array of possible values that could go in that slot.
+  var n = xss.length;
   var combs = [];
   var idxs = [];
   for (var i = 0; i < n; i++) { idxs[i] = 0; }
   while (true) {
-    var perm = [];
+    var comb = [];
     for (var j = 0; j < n; j++) {
-      perm[j] = xs[idxs[j]];
+      comb[j] = xss[j][idxs[j]];
     }
-    combs.push(perm);
+    combs.push(comb);
     var increment = function (k) {
       idxs[k]++;
-      if (idxs[k] >= num) {
+      if (idxs[k] >= xss[k].length) {
         idxs[k] = 0;
         if (k+1 === n) { return true; }
         return increment(k+1);
