ecf0d1c28293f641d288ceda704cd82e3c5ff0e2
sdclibbery <sdclibbery@googlemail.com>
2015-04-15 23:49:55 +0100

    simplify and refactor music module

--- music/music.js
+++ music/music.js
@@ -3,27 +3,18 @@
 // Domain
 
 musis.music = function () {
-  this.nextSolfege = [];
   this.composer = function() { return []; };
 };
 
 musis.music.prototype.nextHarmony = function (solfege) {
-  this.nextSolfege = solfege;
+  this.notes = evaluateNextHarmony(solfege);
+  console.log("Next Harmony: "+this.notes+". tension: "+this.notes.tension);
+  this.composer = musis.compose.blockChords(this.notes);
 };
 
 musis.music.prototype.beat = function (metronome, perform) {
-  if (this.nextSolfege.length > 0) {
-    this.toNextHarmony();
-    this.nextSolfege = [];
-  }
   var events = this.composer(metronome.nextBeatAt(), metronome.beatDuration()); // Compose for the next beat
   perform(events, this.notes);
-};
-
-musis.music.prototype.toNextHarmony = function () {
-  this.notes = evaluateNextHarmony(this.nextSolfege);
-  console.log("Next Harmony: "+this.notes+". tension: "+this.notes.tension);
-  this.composer = musis.compose.blockChords(this.notes);
 };
 
 var evaluateNextHarmony = function (nextSolfege) {
