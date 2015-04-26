d458388a18aa41e7e9d1c8b1f71a1af0d48a9d10
sdclibbery <sdclibbery@googlemail.com>
2015-04-26 23:36:46 +0100

    consider all possible third 'stackings'
    and so get the best representation as stacked thirds, and so the real
    root of the chord (IF it is actually a stacked third chord)

--- music/analysis.js
+++ music/analysis.js
@@ -20,14 +20,59 @@ musis.music.analyse = function (solfege) {
     if (diff < 0) { diff += 7; }
     if (diff % 2 === 1) { diff += 7; }
     var numThirds = diff / 2;
-    if (numThirds > 3) { numThirds -= 7; } // Broken: need to consider all possible arrangements of the input solfege (eg Cmaj9 gets wrong root)
-    thirds.push({ solfege: s, distance: numThirds, toString: function () {return this.distance+':'+this.solfege;} });
+    thirds.push({ solfege: s, distance: numThirds });
   });
-  thirds.sort(function (a, b) { return a.distance - b.distance; });
-  var root = thirds[0].solfege;
-  var hasThird = thirds[1].distance === thirds[0].distance + 1; // Probably only valid to consider as stacked thirds if it has a third
+  var combs = combinations(thirds.map(function (third) {
+    return [
+      { solfege: third.solfege, distance: third.distance },
+      { solfege: third.solfege, distance: third.distance+7 },
+      { solfege: third.solfege, distance: third.distance-7 }
+    ];
+  }));
+  var best = thirds[0];
+  var bestScore = 1e10;
+  combs.map(function (thirds) {
+    var score = 0;
+    thirds.sort(function (a, b) { return a.distance - b.distance; });
+    for (var i = 0; i < thirds.length; i++) {
+      for (var j = i; j < thirds.length; j++) {
+        score += Math.abs(thirds[j].distance - thirds[i].distance);
+      }
+    }
+    if (score < bestScore) {
+      bestScore = score;
+      best = thirds;
+    }
+  });
+  var root = best[0].solfege;
+  var hasThird = best[1].distance === best[0].distance + 1; // Probably only valid to consider as stacked thirds if it has a third
 
-  return thirds.toString();
+  return root;
+};
+
+
+var combinations = function (xss) { // Takes an array with one entry per slot in the output. Each entry is another array of possible values that could go in that slot.
+  var n = xss.length;
+  var combs = [];
+  var idxs = [];
+  for (var i = 0; i < n; i++) { idxs[i] = 0; }
+  while (true) {
+    var comb = [];
+    for (var j = 0; j < n; j++) {
+      comb[j] = xss[j][idxs[j]];
+    }
+    combs.push(comb);
+    var increment = function (k) {
+      idxs[k]++;
+      if (idxs[k] >= xss[k].length) {
+        idxs[k] = 0;
+        if (k+1 === n) { return true; }
+        return increment(k+1);
+      }
+    }
+    if (increment(0)) { break; }
+  }
+  return combs;
 };
 
 })();
