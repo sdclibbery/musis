51117e48f10825d7307f0e3c78d5d548fce35aaa
sdclibbery <sdclibbery@googlemail.com>
2015-05-09 21:42:39 +0100

    first level

--- game.js
+++ game.js
@@ -2,14 +2,8 @@
 
 // Domain
 
-musis.game = {};
-
-musis.game.begin = function () {
-  musis.info.title('Play a Tonic Triad');
-};
-
-musis.game.solfegeTriggers = function () {
-  return [
+var solfegeTriggers = {
+  tonicTriad: [
     { value: "do", size: "large", disabled: false },
     { value: "mi", size: "small", disabled: false },
     { value: "sol", size: "large", disabled: false },
@@ -17,14 +11,52 @@ musis.game.solfegeTriggers = function () {
     { value: "re", size: "small", disabled: true },
     { value: "fa", size: "large", disabled: true },
     { value: "la", size: "small", disabled: true }
-  ];
+  ],
+  all: [
+    { value: "do", size: "large", disabled: false },
+    { value: "mi", size: "small", disabled: false },
+    { value: "sol", size: "large", disabled: false },
+    { value: "ti", size: "small", disabled: false },
+    { value: "re", size: "small", disabled: false },
+    { value: "fa", size: "large", disabled: false },
+    { value: "la", size: "small", disabled: false }
+  ]
+};
+
+var levels = [
+  {
+    title: 'Play a Tonic Triad',
+    solfegeTriggers: solfegeTriggers.tonicTriad,
+    complete: function (analysis) {
+      return (analysis.harmony.root === 'do' && analysis.harmony.hasTriad);
+    }
+  },
+  {
+    title: 'Finished!',
+    solfegeTriggers: solfegeTriggers.all,
+    complete: function (analysis) { return false; }
+  }
+];
+
+musis.game = {
+  levelIdx: 0,
+  level: levels[0]
+};
+
+musis.game.begin = function () {
+  musis.info.title(this.level.title);
+};
+
+musis.game.solfegeTriggers = function () {
+  return this.level.solfegeTriggers;
 };
 
 musis.game.nextHarmony = function (analysis) {
-  if (analysis.harmony.root === 'do' && analysis.harmony.hasTriad) {
-    console.log('done!');
+  if (this.level.complete(analysis)) {
+    this.levelIdx++;
+    this.level = levels[this.levelIdx];
+    this.begin();
   }
 };
-
 
 })();
