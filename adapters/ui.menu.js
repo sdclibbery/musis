f9ed87b0c0dd4ff752b3c7ff1abf1c14cb835070
sdclibbery <sdclibbery@googlemail.com>
2015-05-27 22:48:51 +0100

    first menu structure

--- adapters/ui.menu.js
+++ adapters/ui.menu.js
@@ -6,8 +6,18 @@ musis.ui = musis.ui || {};
 
 musis.ui.menu = {};
 
-musis.ui.menu.item = function (name) {
-  document.getElementById('menu').innerHTML = '<div class="menu-item">'+name+'</div>';
+musis.ui.menu.clear = function () {
+  var menu = document.getElementById('menu');
+  menu.innerHTML = '';
+};
+
+musis.ui.menu.item = function (name, action) {
+  var menu = document.getElementById('menu');
+  var item = document.createElement('div');
+  item.className = 'menu-item';
+  item.innerHTML = name;
+  item.onclick = action;
+  menu.appendChild(item);
 };
 
 })();
--- menus.js
+++ menus.js
@@ -2,13 +2,37 @@
 
 // Domain
 
+var menus = {
+  home: {
+    title: 'musis',
+    items: [
+      { name: 'tutorials', action: function () { show(menus.tutorials); } },
+      { name: 'free play' },
+      { name: 'settings' }
+    ]
+  },
+  tutorials: {
+    title: 'tutorials',
+    items: [
+      { name: 'diatonic harmony' },
+      { name: 'chromatic harmony' }
+    ]
+  }
+};
+
 musis.menus = {};
 
 musis.menus.begin = function () {
-  musis.ui.title('');
   musis.ui.hint('');
+  show(menus.home);
+};
 
-  musis.ui.menu.item('blah');
+var show = function (menu) {
+  musis.ui.menu.clear();
+  musis.ui.title(menu.title);
+  menu.items.map(function (item) {
+    musis.ui.menu.item(item.name, item.action);
+  });
 };
 
 })();
