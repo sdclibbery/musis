1204a664455efa97cce8f9ed03e572a098422791
sdclibbery <sdclibbery@googlemail.com>
2015-06-04 23:05:14 +0100

    spec out tests ready for implementation

--- music/voicing_spec.js
+++ music/voicing_spec.js
@@ -18,4 +18,30 @@ describe("voicing", function() {
 
   property("produces four voices", function (ns) { return ns.length === 4; } );
 
+  // property("all of first four pcs are represented", function (ns) { return ; } );
+
+  // property("voices are in order of pitch", function (ns) { return ; } );
+
+  // property("lowest note is bass pc", function (ns) { return ; } );
+
+  // property("bass is in range", function (ns) { return ; } );
+
+  // property("tenor is in range", function (ns) { return ; } );
+
+  // property("alto is in range", function (ns) { return ; } );
+
+  // property("soprano is in range", function (ns) { return ; } );
+
+  // property("either open or closed voicing", function (ns) { return ; } );
+
+  // property("voices do not cross", function (ns) { return ; } );
+
+  // property("voices do not overlap", function (ns) { return ; } );
+
+  // property("smooth movement", function (ns) { return ; } );
+
+  // property("no consecutive octaves", function (ns) { return ; } );
+
+  // property("no consecutive fifths", function (ns) { return ; } );
+
 });
