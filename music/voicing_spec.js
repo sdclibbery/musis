710a8c62c1dce82cb9e70a4b5aa8f234d27dcd78
sdclibbery <sdclibbery@googlemail.com>
2015-06-04 23:24:34 +0100

    implement test

--- music/voicing_spec.js
+++ music/voicing_spec.js
@@ -10,38 +10,44 @@ var arbPcs = jsc.nearray(jsc.elements(['C', 'E', 'G', 'B', 'D', 'F', 'A', 'F#', 
 var property = function (name, cond) {
   jsc.property(name, arbPcs, function (pcs) {
     var notes = musis.voicing.assignToVoices(pcs);
-    return !!cond(notes);
+    return !!cond(pcs, notes);
   });
 };
 
 describe("voicing", function() {
 
-  property("produces four voices", function (ns) { return ns.length === 4; } );
+  property("produces four voices", function (pcs, ns) { return ns.length === 4; } );
 
-  // property("all of first four pcs are represented", function (ns) { return ; } );
+  property("all of first four pcs are represented", function (pcs, ns) {
+    return pcs.slice(0, 4).every(function (pc) {
+      return ns.some(function (n) {
+        return n.pitchClass === pc; 
+      });
+    });
+  } );
 
-  // property("voices are in order of pitch", function (ns) { return ; } );
+  // property("voices are in order of pitch", function (pcs, ns) { return ; } );
 
-  // property("lowest note is bass pc", function (ns) { return ; } );
+  // property("lowest note is bass pc", function (pcs, ns) { return ; } );
 
-  // property("bass is in range", function (ns) { return ; } );
+  // property("bass is in range", function (pcs, ns) { return ; } );
 
-  // property("tenor is in range", function (ns) { return ; } );
+  // property("tenor is in range", function (pcs, ns) { return ; } );
 
-  // property("alto is in range", function (ns) { return ; } );
+  // property("alto is in range", function (pcs, ns) { return ; } );
 
-  // property("soprano is in range", function (ns) { return ; } );
+  // property("soprano is in range", function (pcs, ns) { return ; } );
 
-  // property("either open or closed voicing", function (ns) { return ; } );
+  // property("either open or closed voicing", function (pcs, ns) { return ; } );
 
-  // property("voices do not cross", function (ns) { return ; } );
+  // property("voices do not cross", function (pcs, ns) { return ; } );
 
-  // property("voices do not overlap", function (ns) { return ; } );
+  // property("voices do not overlap", function (pcs, ns) { return ; } );
 
-  // property("smooth movement", function (ns) { return ; } );
+  // property("smooth movement", function (pcs, ns) { return ; } );
 
-  // property("no consecutive octaves", function (ns) { return ; } );
+  // property("no consecutive octaves", function (pcs, ns) { return ; } );
 
-  // property("no consecutive fifths", function (ns) { return ; } );
+  // property("no consecutive fifths", function (pcs, ns) { return ; } );
 
 });
