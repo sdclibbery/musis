7c307dfdc6303085410309d6e609e43fabf0dddd
sdclibbery <sdclibbery@googlemail.com>
2015-06-12 22:52:43 +0100

    Fix tuning renames so they actually work :-/

--- music/tuning.js
+++ music/tuning.js
@@ -12,33 +12,32 @@ var G = C*fifth; // dominant generator
 var F = C/fifth*2; // sub-dominant generator
 var third = 5/4; // major third
 
-var tunings = {
-  equal12tet: {
-    C: 440*Math.pow(2, -9/12),
-    D: 440*Math.pow(2, -7/12),
-    Eb: 440*Math.pow(2, -4/12),
-    E: 440*Math.pow(2, -5/12),
-    F: 440*Math.pow(2, -4/12),
-    'F#': 440*Math.pow(2, -5/12),
-    G: 440*Math.pow(2, -2/12),
-    Ab: 440*Math.pow(2, -1/12),
-    A: 440*Math.pow(2, 0/12),
-    Bb: 440*Math.pow(2, 1/12),
-    B: 440*Math.pow(2, 2/12)
-  },
- just5limit: {
-    C: C,
-    D: G*fifth/2,
-    Eb: G/third,
-    E: C*third,
-    F: F,
-    'F#': G*fifth/2*third,
-    G: G,
-    Ab: C/third*2,
-    A: F*third,
-    Bb: F/fifth*2,
-    B: G*third
-  }
+var tunings = {};
+tunings[equal12tet] = {
+  C: 440*Math.pow(2, -9/12),
+  D: 440*Math.pow(2, -7/12),
+  Eb: 440*Math.pow(2, -4/12),
+  E: 440*Math.pow(2, -5/12),
+  F: 440*Math.pow(2, -4/12),
+  'F#': 440*Math.pow(2, -5/12),
+  G: 440*Math.pow(2, -2/12),
+  Ab: 440*Math.pow(2, -1/12),
+  A: 440*Math.pow(2, 0/12),
+  Bb: 440*Math.pow(2, 1/12),
+  B: 440*Math.pow(2, 2/12)
+};
+tunings[just5limit] = {
+  C: C,
+  D: G*fifth/2,
+  Eb: G/third,
+  E: C*third,
+  F: F,
+  'F#': G*fifth/2*third,
+  G: G,
+  Ab: C/third*2,
+  A: F*third,
+  Bb: F/fifth*2,
+  B: G*third
 };
 
 musis.tuning = {
